7a059f3a9dec673b268ea3687cc8798d0d79c3d7
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-03-15 14:05:20 -0300

    feat: stablish dependency between node and mysql

--- node/index.js
+++ node/index.js
@@ -4,6 +4,15 @@ const mysql = require("mysql2/promise")
 const app = express()
 const PORT = process.env.PORT || 8080
 
+const createConnection = async () => {
+  return await mysql.createConnection({
+    host: 'db',
+    user: 'root',
+    password: 'root',
+    database: 'fulldb',
+  });
+}
+
 const getRandomName = () => {
   const randomNames = ["zeca roceiro", "zé carroceiro", "limãozinho", "michael jackson", "mel gibsa"]
   return randomNames[Math.floor(Math.random() * randomNames.length)]
@@ -12,17 +21,13 @@ const getRandomName = () => {
 app.get("/", async (req, res) => {
   let html = "<h1>Full Cycle Rocks!</h1>"
   
-  const connection = await mysql.createConnection({
-    host: 'db',
-    user: 'root',
-    password: 'root',
-    database: 'fulldb',
-  });
-
+  const connection = await createConnection()
+  
   await connection.execute("INSERT INTO people (name) VALUES (?)", [getRandomName()])
+  await connection.commit()
+
   const [people] = await connection.execute("SELECT * FROM people")
   await connection.end()
-  console.log(people)
   
   let names = ""
   for (let person of people) {
