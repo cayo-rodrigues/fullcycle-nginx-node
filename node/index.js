9051d2640318edfb48d564d3421a40f164dba328
Cayo Rodrigues <cayo.rodrigues1914@gmail.com>
2023-03-16 09:31:27 -0300

    ops: set nginx as reverse proxy for node api

--- node/index.js
+++ node/index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mysql = require("mysql2/promise")
 
 const app = express()
-const PORT = process.env.PORT || 8080
+const PORT = process.env.PORT || 3000
 
 const createConnection = async () => {
   return await mysql.createConnection({
